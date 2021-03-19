a8bcc557c9467a6d290434a9bfdebe8b92bb1524
David Espí Hernández <davidespihernandez@gmail.com>
2021-03-19 20:50:33 +0100

    Add models: bid and item

--- bids/models.py
+++ bids/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    best_bid = models.OneToOneField(
+        "Bid", on_delete=models.SET_NULL, null=True, related_name="best_item"
+    )
+
+
+class Bid(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
