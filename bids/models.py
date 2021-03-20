3bec75aab3314871b6dc164d4145725c4d5996bc
David Espí Hernández <davidespihernandez@gmail.com>
2021-03-20 08:00:36 +0100

    Modify bids

--- bids/models.py
+++ bids/models.py
@@ -14,3 +14,9 @@ class Bid(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="bids")
     amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [["user", "item"], ["item", "amount"]]
+        ordering = ["-amount", "-last_updated", "-pk"]
