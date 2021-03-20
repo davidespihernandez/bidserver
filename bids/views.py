14f32766d237550147e4d7f4950afdad557b58e7
David Espí Hernández <davidespihernandez@gmail.com>
2021-03-20 21:54:17 +0100

    Basic views and urls

--- bids/views.py
+++ bids/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from bids.models import Bid, Item
+from bids.serializers import BidSerializer, ItemSerializer
+
+
+class BidViewSet(ModelViewSet):
+    queryset = Bid.objects.all()
+    serializer_class = BidSerializer
+
+
+class ItemViewSet(ModelViewSet):
+    queryset = Item.objects.select_related("best_bid").all()
+    serializer_class = ItemSerializer
--- bidserver/urls.py
+++ bidserver/urls.py
@@ -5,10 +5,18 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from rest_framework.routers import DefaultRouter
+
+from bids.views import ItemViewSet, BidViewSet
+
+router = DefaultRouter()
+router.register(r"api/items", ItemViewSet)
+router.register(r"api/bids", BidViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-]
+] + router.urls
